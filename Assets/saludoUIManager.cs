55cbf82d3c61ff426b57385a767dc4b8fe2fff1d
OliNaider <166539084+OliNaider@users.noreply.github.com>
2025-05-06 12:15:32 -0300

    terminado

---
+++ Assets/saludoUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class saludoUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI TxTSaludo;
+    public TMP_InputField inputNombre;
+
+    string nombreIngresado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TxTSaludo.text = "";
+
+
+    }
+
+   public void Saludar()
+    {
+        TxTSaludo.text = "Hola " + inputNombre.text;
+    } 
+}
